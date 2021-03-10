647da0a8333a72dc1d145e28c2c3eb96155fd000
HoooDY <sinbbgirl1@naver.com>
2021-03-10 16:48:06 +0900

    tutorial

--- Graphql/db.js
+++ Graphql/db.js
@@ -1,30 +1,48 @@
-export const people = [{
+let movies = [{
         id: 1,
-        name: "GEUNTAE",
-        age: 20,
-        gender: "man"
+        name: "머신건의 반격",
+        score: 1
     },
     {
         id: 2,
-        name: "a",
-        age: 25,
-        gender: "female"
+        name: "윾쾌한 던짐",
+        score: 2
     },
     {
         id: 3,
-        name: "b",
-        age: 24,
-        gender: "man"
+        name: "코롤로나",
+        score: 99
     },
     {
         id: 4,
-        name: "CE",
-        age: 27,
-        gender: "feman"
+        name: "킬링키",
+        score: 15
     },
 ];
 
+export const getMovies = () => movies;
+
 export const getByID = id => {
-    const filteredPeople = people.filter(person => id === person.id)
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => id === movie.id)
+    return filteredMovies[0];
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => id !== movie.id)
+    if (movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name,
+        score
+    }
+    movies.push(newMovie)
+    return newMovie
 }
--- Graphql/resolvers.js
+++ Graphql/resolvers.js
@@ -1,14 +1,18 @@
 import {
-    people,
-    getByID
+    getMovies,
+    getByID,
+    addMovie,
+    deleteMovie
 } from './db'
 
 const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, {
-            id
-        }) => getByID(id)
+        movies: () => getMovies(),
+        movie: (_, {id}) => getByID(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score),
+        deleteMovie: (_, {id}) => deleteMovie(id)
     }
 };
 
